70e010e02ed7c5c759feea231e93f69f8d2d1621
Andrei Dzebysh <presnia@yahoo.com>
2020-11-04 16:56:46 +0300

    Divided into several modules

---
+++ modules/nodesHierarchy.js
@@ -0,0 +1,45 @@
+const nodesHierarchy = {
+  label: "EventTarget",
+  children: [
+    {
+      label: "Node",
+      children: [
+        {
+          label: "Text",
+          children: [],
+        },
+        {
+          label: "Comment",
+          children: [],
+        },
+        {
+          label: "Element",
+          children: [
+            {
+          label: "SVGElement",
+          children: [],
+        },{
+          label: "HTMLElement",
+          children: [
+            {
+          label: "HTMLInputElement",
+          children: [],
+        },
+        {
+          label: "HTMLBodyElement",
+          children: [],
+        },
+        {
+          label: "HTMLAnchorElement",
+          children: [],
+        },
+          ],
+        },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+export { nodesHierarchy };
--- src/index.js
+++ src/index.js
@@ -1,137 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-console */
 
-const browserTree = {
-  label: "Window",
-  children: [
-    {
-      label: "DOM - Document Object Model; DOM Tree",
-      children: [
-        {
-          label: "document",
-          children: [],
-        },
-        {
-          label: "CSSOM - CSS Object Model",
-          children: [],
-        },
-      ],
-    },
-    {
-      label: "BOM - Browser Object Model",
-      children: [
-        {
-          label: "navigator",
-          children: [],
-        },
-        {
-          label: "screen",
-          children: [],
-        },
-        {
-          label: "location",
-          children: [],
-        },
-        {
-          label: "frames",
-          children: [],
-        },
-        {
-          label: "history",
-          children: [],
-        },
-        {
-          label: "XMLHttpReques",
-          children: [],
-        },
-      ],
-    },
-    {
-      label: "JavaScript",
-      children: [
-        {
-          label: "Object",
-          children: [],
-        },
-        {
-          label: "Array",
-          children: [],
-        },
-        {
-          label: "Function",
-          children: [],
-        },
-        {
-          label: "...",
-          children: [],
-        },
-      ],
-    },
-  ],
-};
+import { browserTree } from "../modules/browserTree.js";
+import { nodesHierarchy } from "../modules/nodesHierarchy.js";
+import { renderTree } from "../modules/renderTree.js";
 
-// рекурсия
-
-const nodesHierarchy = {
-  label: "EventTarget",
-  children: [
-    {
-      label: "Node",
-      children: [
-        {
-          label: "Text",
-          children: [],
-        },
-        {
-          label: "Comment",
-          children: [],
-        },
-        {
-          label: "Element",
-          children: [
-            {
-          label: "SVGElement",
-          children: [],
-        },{
-          label: "HTMLElement",
-          children: [
-            {
-          label: "HTMLInputElement",
-          children: [],
-        },
-        {
-          label: "HTMLBodyElement",
-          children: [],
-        },
-        {
-          label: "HTMLAnchorElement",
-          children: [],
-        },
-          ],
-        },
-          ],
-        },
-      ],
-    },
-  ],
-};
-
-function renderTree(treeArray) {
-  if (treeArray.length === 0) return null;
-
-  const rootUlElement = document.createElement("ul");
-
-  treeArray.forEach((node) => {
-    const liElement = document.createElement("li");
-    liElement.innerHTML = node.label;
-    rootUlElement.append(liElement);
-
-    const subTree = renderTree(node.children);
-    if (subTree !== null) rootUlElement.append(subTree);
-  });
-
-  return rootUlElement;
-}
+const trees = wholeArray();
 
 function wholeArray() {
   let arr1 = Object.values(browserTree);
@@ -140,9 +14,6 @@ function wholeArray() {
 
   return result;
 }
-const trees = wholeArray();
-
-const complexArray = [1, 2, 3, [4, 5, 6, [7, 8, 9], 10, [11, 12]]];
 
 function flattenArray(array) {
   let result = [];
@@ -168,9 +39,6 @@ export function renderPage() {
   const rootDiv = document.getElementById("root");
   if (tree !== null) rootDiv.append(tree);
 
-  const flatArray = flattenArray(complexArray);
-  rootDiv.append(renderArray(flatArray));
-
   const flatOurArray = flattenArray(trees);
   rootDiv.append(renderArray(flatOurArray));
 }
@@ -180,77 +48,4 @@ export function buildPage() {
     name: "Js",
     place: "IT-Academy",
   };
-
-
-
-
-
-
-
-
-
-
-
-  // examples
-
-  document.body.sayHello = function () {
-    alert("Hi!!!!");
-  };
-
-  const rootDiv = document.getElementById("root");
-  rootDiv.style.background = "green";
-  rootDiv.style.padding = "10px";
-
-  const topicList = document.getElementById("topic-list");
-  topicList.style.background = "red";
-
-  const sl1 = document.querySelector(".sub-list1");
-  const r1 = sl1.querySelector(".for-query");
-
-  const sl2 = document.querySelector(".sub-list2");
-  const r2 = sl2.querySelector(".for-query");
-
-  const sl3 = document.querySelector(".sub-list3");
-  const r3 = sl3.querySelector(".for-query");
-
-  const subItem = document.querySelectorAll(".function-sub-item");
-
-  sl2.childNodes.forEach((node) => {
-    if (node.innerText !== undefined) {
-      node.style.background = "blue";
-    }
-  });
-
-  sl2
-    .querySelectorAll("li")
-    .forEach((node) => (node.style.background = "yellow"));
-
-  for (let i = 0; i < sl2.children.length; i++) {
-    sl2.children[i].style.background = "gray";
-  }
-
-  // alert(document.body.id);
-  // alert(document.body.getAttribute("myCoolAttribute"));
-  const textNode = document.createTextNode(" My text&amp;");
-
-  const infoMessage = document.createElement("div");
-  infoMessage.className = "alert";
-  infoMessage.innerHTML = "<strong>&amp;This is info message.</strong>";
-
-  infoMessage.append(textNode);
-
-  const bodyInfoMessage = document.createElement("div");
-  bodyInfoMessage.className = "alert";
-  bodyInfoMessage.innerHTML = "This is info message.";
-
-  document.body.append(bodyInfoMessage);
-
-  rootDiv.prepend(infoMessage);
-
-  const clone = infoMessage.cloneNode(true);
-  clone.innerHTML = "I am a clone";
-
-  rootDiv.append(clone);
-
-  // rootDiv.remove();
 }
